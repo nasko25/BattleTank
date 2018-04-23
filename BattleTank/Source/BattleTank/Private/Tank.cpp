e01da5a392dbd520c09514bfb750f9ae414f2d99
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2018-04-23 19:31:04 +0300

    Projectile moves.

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -47,15 +47,16 @@ void ATank::SetTurretReference(UTankTurret* TurretToSet) {
 }
 
 void ATank::Fire() {
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f:Tank fires"), Time)
-
+	/*auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f:Tank fires"), Time)*/
 	if (!Barrel) { return; }
 
 	// Spawn a projectile in the socket location on the barrel
-	GetWorld()->SpawnActor<AProjectile>(
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 		ProjectileBlueprint,
 		Barrel->GetSocketLocation(FName("Projectile")),
 		Barrel->GetSocketRotation(FName("Projectile"))
-		);
+	);
+	
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
