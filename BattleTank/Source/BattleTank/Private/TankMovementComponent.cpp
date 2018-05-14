e80ab5e5a63dea25029f7bcc6a02b34729960b22
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2018-05-14 17:30:12 +0300

    Finilizing TankMovementComponent class.

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -20,7 +20,6 @@ void UTankMovementComponent::IntendTurnRight(float Throw) {
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
-	// TODO prevent double-speed due to dual control use
 }
 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) {
