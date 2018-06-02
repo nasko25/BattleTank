7c56d81eea1a8418e32e7e186847f61069f7739f
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2018-06-02 13:20:17 +0300

    Added a OnHit() event on the tracks with C++.

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -4,6 +4,14 @@
 
 UTankTrack::UTankTrack() {
 	PrimaryComponentTick.bCanEverTick = true;
+
+}
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit) {
+	UE_LOG(LogTemp, Warning, TEXT("Track hit"))
+}
+void UTankTrack::BeginPlay() {
+	Super::BeginPlay();
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
 }
 
 void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) {
