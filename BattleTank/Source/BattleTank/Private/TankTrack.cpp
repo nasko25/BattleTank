3a4e6bf5b0912da31bb3d72857ed67b0ca022841
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2018-06-03 14:51:13 +0300

    Fixed the weird flying bug.

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -3,37 +3,46 @@
 #include "TankTrack.h"
 
 UTankTrack::UTankTrack() {
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 }
 void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit) {
-	UE_LOG(LogTemp, Warning, TEXT("Track hit"))
+	DriveTrack();
+	ApplySidewaysForce();
+	// reset the throttle
+	CurrentThrottle = 0;
 }
 void UTankTrack::BeginPlay() {
 	Super::BeginPlay();
 	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
 }
 
-void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	// Calculate the slipage speed
+
+void UTankTrack::ApplySidewaysForce()
+{
+	// Calculate the slipage speed 	// Work-out the required acceleration this frame to correct 
 	auto SlipageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
-	// Work-out the required acceleration this frame to correct 
-	auto CorrectionAcceleration = -SlipageSpeed / DeltaTime * GetRightVector(); 
+
+	auto DeltaTime = GetWorld()->GetDeltaSeconds();
+	auto CorrectionAcceleration = -SlipageSpeed / DeltaTime * GetRightVector();
 	// Calculate and apply sideways force ( F = ma)
-	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent()); 
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
 	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Two tracks
 	TankRoot->AddForce(CorrectionForce);
 }
 
 void UTankTrack::SetThrottle(float Throttle) {
-//	auto Time = GetWorld()->GetTimeSeconds();
+	CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -2, 2);
+}
+
+void UTankTrack::DriveTrack() {
+	//	auto Time = GetWorld()->GetTimeSeconds();
 	/*auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle)*/
 
-	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
-	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent()); 
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	if (TankRoot)
 	{
 		TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
