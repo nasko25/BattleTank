876e6a9c47546b855937c5143a26b4b7e09e916d
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2018-06-27 19:20:19 +0300

    Bug fixes.

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -57,7 +57,7 @@ void ATankAIController::Tick(float DeltaTime) {
 void ATankAIController::AimTowardsCrosshair() {
 	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
 	auto ControlledTank = GetPawn();
-	if (!ensure(PlayerTank && ControlledTank)) { return; }
+	if (!(PlayerTank && ControlledTank)) { return; }
 	// Move towards the player
 	MoveToActor(PlayerTank, AcceptanceRadius);
 
